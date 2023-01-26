6eb96afd7513efee83c2e15990a3e8014f917936
2xer <74143110+2xer@users.noreply.github.com>
2023-01-26 05:19:11 +0200

    Add javadoc comments and organize source file

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
+/**
+ * Main class
+ */
 public class Main {
+	/**
+	 * main method
+	 *
+	 * @param args Arguments passed from CLI
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 	}
